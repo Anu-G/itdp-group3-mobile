4c116a587c8e46fcb8275b8e27def7d21f4352fc
Christiane Tamiyang Silalahi <christiane.silalahi@yahoo.com>
2022-09-22 11:19:49 +0700

    [FIX] some navbar

--- src/navigation/TabRouter.js
+++ src/navigation/TabRouter.js
@@ -1,7 +1,10 @@
-import { AntDesign, Entypo, FontAwesome } from "@expo/vector-icons";
+import { AntDesign, Entypo, FontAwesome, FontAwesome5, Fontisto, Foundation, Octicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { AddPost } from "../features/AddPost/AddPost";
 import { Home } from "../features/Home/Home";
+import { BusinessProfile } from "../features/Profile/BusinessProfile";
 import { Setting } from "../features/Setting/Setting";
 import { ROUTE } from "../shared/constants/NavigationConstants";
 import { useTheme } from "../shared/context/ThemeContext";
@@ -11,26 +14,53 @@ export const TabRouter = _ => {
    const theme = useTheme();
    const style = styling(theme.state.style);
 
+   const [profile, setProfile] = useState('')
+
    return (
-      <Tab.Navigator screenOptions={({ route }) => ({
+      <Tab.Navigator initialRouteName={ROUTE.MAIN} screenOptions={({ route }) => ({
          tabBarIcon: ({ focused, color, size }) => {
             switch (route.name) {
                case ROUTE.HOME:
-                  return <Entypo name="home" size={style.tabIcon.size} color={color} />
-               case ROUTE.SETTING:
-                  return <FontAwesome name="gear" size={style.tabIcon.size} color={color} />
+                  return (
+                  <View style={style.iconCtn}>
+                     <Entypo name="home" size={style.tabIcon.size} color={color} style={focused ? style.textShadow : ''}/>
+                  </View>
+                  )
+               case ROUTE.ADD_POST:
+
+                  return (
+                  <View style={style.iconCtn}>
+                     <Foundation name='magnifying-glass' size={style.tabIcon.size} color={color} style={focused ? style.textShadow : ''} />
+                  </View>
+                  )
+                  case ROUTE.SETTING:
+                     return (
+                     <View style={style.iconCtn}>
+                        <Octicons name="bell-fill" size={style.tabIcon.size -4} color={color} style={focused ? style.textShadow : ''} />
+                     </View>
+                     )
+               case ROUTE.BUSINESS_PROFILE:
+                  return (
+                     <View style={focused ? [style.imageCtn] : ''}>
+                        <Image source={require('../../assets/images/user-default.png')} style={style.imageStyle}/>
+                     </View>                    
+                  )
                default:
-                  <AntDesign name="exclamationcircleo" size={style.tabIcon.size} color={color} />
+                  <AntDesign name="exclamationcircleo" size={style.tabIcon.size } color={color} />
             }
          },
          tabBarActiveTintColor: style.tabIcon.activeColor,
          tabBarInactiveTintColor: style.tabIcon.inactiveColor,
          tabBarStyle: { ...style.tabBar },
-         tabBarLabel: () => null
+         tabBarLabel: () => null,
       })}>
          <Tab.Group screenOptions={{ headerShown: false }} >
             <Tab.Screen name={ROUTE.HOME} component={Home} />
+
+            {/* dummy */}
+            <Tab.Screen name={ROUTE.ADD_POST} component={AddPost} />
             <Tab.Screen name={ROUTE.SETTING} component={Setting} />
+            <Tab.Screen name={ROUTE.BUSINESS_PROFILE} component={BusinessProfile} />
          </Tab.Group>
       </Tab.Navigator>
    )
@@ -40,6 +70,53 @@ const styling = (theme) => StyleSheet.create({
    tabIcon: { ...theme?.tabIcon },
    tabBar: {
       backgroundColor: theme?.colors?.tabBackground,
-      height: 64
+      minHeight: 64,
+      // paddingTop: 12,
+      // justifyContent: "center",
+      },
+   tabShadow: {
+      shadowColor: "#000000",
+      shadowOffset: {
+      width: 0,
+      height: 8,
+      },
+      shadowOpacity:  0.21,
+      shadowRadius: 8.19,
+      elevation: 11
+   },
+   textShadow: {
+      textShadowColor: 'rgba(0,0,0, 0.75)',
+      textShadowOffset: {
+         height: 2,
+         // width: 2
+      },
+      textShadowRadius: 4,
+      // backgroundColor: 'red',
+   },
+   imageStyle: {
+      height: 28,
+      width: 28,
+      borderRadius: 20
+   },
+   imageCtn: {
+      borderWidth: 2, 
+      borderRadius: 20, 
+      borderColor: '#FED154',
+      shadowColor: 'rgba(0,0,0)',
+      shadowOffset: {
+         width: 0,
+         height: 4,
+      },
+      shadowRadius: 4,
+      elevation: 2,
+      shadowOpacity: 0.75
+   },
+   iconCtn: {
+      height: 32,
+      width: 32,
+      alignContent: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      // backgroundColor: 'red'
    }
 })
